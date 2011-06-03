494b0af3edb0c6c8eb1633c75f0ef742c2b5054a
Matthew Hooker <mwhooker@gmail.om>
2011-06-03 01:59:18 -0400

    install script

---
+++ install.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+if [ "`echo $0 | cut -c1`" = "/" ]; then
+    DOTFILES_PATH=`dirname $0`
+else
+    DOTFILES_PATH=$(dirname `pwd`/`echo $0 | cut -c3-`)
+fi
+
+if [ -L ~/.screenrc ]; then
+    unlink ~/.screenrc
+fi
+
+if [ -L ~/.gitconfig ]; then
+    unlink ~/.gitconfig
+fi
+
+ln -s ${DOTFILES_PATH}/gitconfig ${HOME}/.gitconfig
+ln -s ${DOTFILES_PATH}/screenrc ${HOME}/.screenrc
