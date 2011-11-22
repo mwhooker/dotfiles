0f73c3b314b5fa6d0cb6843c7b5ebaa841973aca
Matthew Hooker <matt@cloudscaling.com>
2011-11-22 16:41:57 -0500

    add tmux config.

--- install.sh
+++ install.sh
@@ -18,7 +18,12 @@ if [ -L ~/.ssh/config ]; then
     unlink ~/.ssh/config
 fi
 
+if [ -L ~/.tmux.conf ]; then
+    unlink ~/.tmux.conf
+fi
+
 ln -s ${DOTFILES_PATH}/gitconfig ${HOME}/.gitconfig
 ln -s ${DOTFILES_PATH}/screenrc ${HOME}/.screenrc
 ln -s ${DOTFILES_PATH}/sshconfig ${HOME}/.ssh/config
+ln -s ${DOTFILES_PATH}/tmux.conf ${HOME}/.tmux.conf
 git config --global core.excludesfile ${DOTFILES_PATH}/gitignore
