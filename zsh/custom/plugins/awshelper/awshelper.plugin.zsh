ffd52e95bb3ce3367ecc5fbec20476fa9eb1ec24
Matthew Hooker <mwhooker@gmail.com>
2015-09-23 13:18:39 -0700

    don't forward ssh agent by default. allow ec2ssh to take ssh params

--- zsh/custom/plugins/awshelper/awshelper.plugin.zsh
+++ zsh/custom/plugins/awshelper/awshelper.plugin.zsh
@@ -1,4 +1,5 @@
 function ec2ssh() {
     dns=$(aws ec2 describe-instances --output text --instance-id $1 --query "Reservations[0].Instances[0].PublicDnsName")
-    ssh core@${dns}
+    shift
+    ssh core@${dns} $@
 }
