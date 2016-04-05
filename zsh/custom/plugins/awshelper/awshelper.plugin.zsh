bccb60e3a5ced431869207443995df242d056ce0
Matthew Hooker <mwhooker@gmail.com>
2016-04-05 10:53:09 -0700

    support sshing and commands

--- zsh/custom/plugins/awshelper/awshelper.plugin.zsh
+++ zsh/custom/plugins/awshelper/awshelper.plugin.zsh
@@ -1,5 +1,9 @@
 function ec2ssh() {
     dns=$(aws ec2 describe-instances --output text --instance-id $1 --query "Reservations[0].Instances[0].PublicDnsName")
     shift
-    ssh core@${dns} $@
+    if [[ $# > 0 ]]; then
+        ssh -n core@${dns} $@
+    else
+        ssh core@${dns} $@
+    fi
 }
