616aab073cb9962f1a9dd9a066d63a6fa139041d
Matthew Hooker <mwhooker@gmail.com>
2016-05-02 12:19:57 -0700

    use system aws

--- zsh/custom/plugins/awshelper/awshelper.plugin.zsh
+++ zsh/custom/plugins/awshelper/awshelper.plugin.zsh
@@ -1,8 +1,9 @@
 function ec2ssh() {
-set +x
-    dns=$(aws ec2 describe-instances --output text --instance-id $1 --query "Reservations[0].Instances[0].PublicDnsName")
+    AWS=/usr/local/bin/aws
+    set +x
+    dns=$($AWS ec2 describe-instances --output text --instance-id $1 --query "Reservations[0].Instances[0].PublicDnsName")
     if [ $? -ne 0 ]; then
-        dns=$(aws ec2 describe-instances --region us-west-1 --output text --instance-id $1 --query "Reservations[0].Instances[0].PublicDnsName")
+        dns=$($AWS ec2 describe-instances --region us-west-1 --output text --instance-id $1 --query "Reservations[0].Instances[0].PublicDnsName")
     fi
     shift
     if [[ $# > 0 ]]; then
